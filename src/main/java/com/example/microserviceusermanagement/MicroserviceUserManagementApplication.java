dcb2c74ab4b201b8c111c5fcb5259724d70199c2
prudhvistunner <bhojanapallip1@udayton.edu>
2023-03-20 20:28:47 -0400

    Enabled eureka service in user service

--- src/main/java/com/example/microserviceusermanagement/MicroserviceUserManagementApplication.java
+++ src/main/java/com/example/microserviceusermanagement/MicroserviceUserManagementApplication.java
@@ -2,8 +2,10 @@ package com.example.microserviceusermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceUserManagementApplication {
 
 	public static void main(String[] args) {
